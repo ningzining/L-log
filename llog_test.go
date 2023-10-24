9429d365b7987fa2c760da98c030feaf14f7113a
liningning <ozz@grizzlychina.com>
2023-10-24 10:30:29 +0800

    feature: 日志库封装

---
+++ llog_test.go
@@ -0,0 +1,30 @@
+package L_log
+
+import (
+	"go.uber.org/zap"
+	"testing"
+)
+
+func TestDebug(t *testing.T) {
+	Debug("hello world", zap.String("name", "cotton"))
+}
+
+func TestInfo(t *testing.T) {
+	Info("hello world", zap.String("name", "cotton"))
+}
+
+func TestWarn(t *testing.T) {
+	Warn("hello world", zap.String("name", "cotton"))
+}
+
+func TestError(t *testing.T) {
+	Error("hello world", zap.String("name", "cotton"))
+}
+
+func TestPanic(t *testing.T) {
+	Panic("hello world", zap.String("name", "cotton"))
+}
+
+func TestFatal(t *testing.T) {
+	Fatal("hello world", zap.String("name", "cotton"))
+}
